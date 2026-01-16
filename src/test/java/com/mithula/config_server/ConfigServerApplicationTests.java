5ca2cb463023b875b62aea5631e222fb3ac9d409
mithula <mithula@dmsswt.com>
2026-01-16 16:03:16 +0530

    config server

---
+++ src/test/java/com/mithula/config_server/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mithula.config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
